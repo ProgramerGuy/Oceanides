52d25ff5ecd289d389e6896882aa4e6e4fed16b2
RaulG <raul.animations@gmail.com>
2022-05-14 22:18:59 -0500

    Se eliminan algunas cosas del template y se implementa librerias AOS

--- WebApp/src/app/app.component.ts
+++ WebApp/src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { AuthService, ScreenService, AppInfoService } from './shared/services';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
   @HostBinding('class') get getClass() {
     return Object.keys(this.screen.sizes).filter(cl => this.screen.sizes[cl]).join(' ');
   }
 
   constructor(private authService: AuthService, private screen: ScreenService, public appInfo: AppInfoService) { }
+
+  ngOnInit(): void {
+    AOS.init();
+  }
 
   isAuthenticated() {
     return this.authService.loggedIn;
